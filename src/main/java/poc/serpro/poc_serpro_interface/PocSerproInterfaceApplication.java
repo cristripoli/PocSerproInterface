31d90765b263b413b0027bf5a305355309885144
Crislaine da Silva Tripoli <ctripoli@daitan.com>
2020-08-19 12:54:17 -0300

    Fisrt commit to create SERPRO POC repository

---
+++ src/main/java/poc/serpro/poc_serpro_interface/PocSerproInterfaceApplication.java
@@ -0,0 +1,13 @@
+package poc.serpro.poc_serpro_interface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSerproInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSerproInterfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/poc/serpro/poc_serpro_interface/PocSerproInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.serpro.poc_serpro_interface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSerproInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
