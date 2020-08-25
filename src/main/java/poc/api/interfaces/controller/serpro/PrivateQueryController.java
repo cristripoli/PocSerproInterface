11e9663d30aaafa9b97c08393294b7337c95e503
ljlopes <ljlopes@daitan.com>
2020-08-25 19:52:28 -0300

    Implements get Scale by number.

--- src/main/java/poc/api/interfaces/controller/serpro/PrivateQueryController.java
+++ src/main/java/poc/api/interfaces/controller/serpro/PrivateQueryController.java
@@ -1,27 +1,23 @@
 package poc.api.interfaces.controller.serpro;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import poc.api.interfaces.services.serpro.PrivateQueryService;
 
 @RestController
-@RequestMapping("serpro/private")
+@RequestMapping("v1/serpro/consulta-restrita")
 public class PrivateQueryController {
 	
 	@Autowired
 	private PrivateQueryService serproService;
 
-	@RequestMapping("hello")
-	public String hello() {
-		return "Hello Serpro Private";
-	}
-	
-	@RequestMapping("escalaMaritima")
-	public void escalaMaritima() {
-//		serproService.buildRequest();
-		System.out.println(serproService.privateQuery("/escalas/18000009140"));
-//		serproService.obtainSecuredResource();
+
+	@GetMapping("/escalas/{numeroEscala}")
+	public String escalaMaritima(@PathVariable Long numeroEscala) {
+		return serproService.privateQuery("/escalas/" + numeroEscala);
 	}
 }
